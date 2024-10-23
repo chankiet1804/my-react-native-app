bff5e3aa71c6a241ab74ab32179beb82dddb7bea
chankiet1804 <22520717@gm.uit.edu.vn>
2024-10-23 21:40:02 +0700

    reset porject

--- utils/const.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export const OPENSANS_REGULAR = "OpenSans-Regular";
-
-export const globalStyles = StyleSheet.create({
-    globalFont: {
-        fontFamily: OPENSANS_REGULAR
-    }
-})
